a56d751084f7185de3268f05e0c5d3e0dced936b
edfang64 <125835699+edfang64@users.noreply.github.com>
2023-02-28 13:15:59 +1100

    Add files via upload

--- battery-ui/src/live_value.tsx
+++ battery-ui/src/live_value.tsx
@@ -8,6 +8,15 @@ interface TemperatureProps {
 function LiveValue({ temp } : TemperatureProps) {
 
   let valueColour = 'white';
+//Making the battery temperature value change colours based on the current temperature
+//Red for too hot, blue for too cold
+  if (temp < 20) {
+    valueColour = 'blue';
+  } else if (temp > 80) {
+    valueColour = 'red';
+  } else {
+    valueColour = 'white';
+  }
 
   return (
       <header className="live-value" style={{ color : valueColour }}>
